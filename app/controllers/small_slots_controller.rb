508995d0553cbdce50f4833d401ca27b5d8bbf3c
santucode <epoori.santosh@zensark.com>
2023-02-22 13:04:05 +0530

    deleted unnecessary files and removed unused code

--- app/controllers/small_slots_controller.rb
+++ app/controllers/small_slots_controller.rb
@@ -1,29 +1,7 @@
 class SmallSlotsController < ApplicationController
-    def index
-        @small_slots = SmallSlot.all
-    end
-    
-    def new
-        @small_slot = SmallSlot.new
-        @medium_slot = MediumSlot.new
-    end
+   
 
-    def create
-        if car_params.to_h.fetch_values(:car_size) == ["small_vehicle"]
-            if SmallSlot.all.length < 5
-                @small_slot = SmallSlot.new(car_params)
-                @small_slot.save!
-                redirect_to root_path
-            elsif MediumSlot.all.length < 5
-                @medium_slot = MediumSlot.new(car_params)
-                @medium_slot.save!
-                redirect_to root_path
-            else
-                render :new, status: :unprocessable_entity
-                flash[:message] = "Try Again"
-            end
-        end
-    end
+  
 
     def destroy
         @small_slot = SmallSlot.find(params[:id])
@@ -32,9 +10,7 @@ class SmallSlotsController < ApplicationController
         redirect_back(fallback_location: root_path)
     end
 
-    def car_params
-        params.permit(:car_size)
-    end
+  
 
 
 end
